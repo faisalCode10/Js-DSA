57d56610911065876f88e8611b5781eef0118881
faisalCode10 <faisalimran477@gmail.com>
2022-10-30 20:38:42 +0500

    delete_element_array

---
+++ delete_array.js
@@ -0,0 +1,30 @@
+// Make an array , define the position 
+// using loop to delete element 
+// delete element with text iput and button
+
+// [10,20,30,12,19,92]
+// we want to remove 30 how will we do that 
+// We have two solution one is predefine funtion and one is we write our own logic 
+
+// let data = [30,20,45,76,20,80]
+// let position= 3
+
+// for(let i=position; i<data.length-1; i++){
+//         data[i]=data[i+1]
+//     }
+//     data.length=data.length-1;
+//     console.log(data)
+
+
+
+const RemoveElem = ()=>{
+    let data = [30,20,45,76,20,80]
+    let position= document.getElementById('position').value;
+    position=parseInt(position2)
+
+for(let i=position; i<data.length-1; i++){
+        data[i]=data[i+1]
+    }
+    data.length=data.length-1;
+    console.log(data)
+}
